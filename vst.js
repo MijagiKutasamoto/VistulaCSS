5b21e184b0289624acd5c853415e9919917b479d
MijagiKutasamoto <165821171+MijagiKutasamoto@users.noreply.github.com>
2024-05-05 23:50:14 +0200

    Delete vst.js

--- vst.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.vst-menu__menu-toggle');
-const menu = document.querySelector('.vst-menu__menu');
-
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('is-active');
-});
